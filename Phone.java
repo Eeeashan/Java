6f868c3d27ec7a6d35a3e87f332dafd8a1f98893
Raiat Hasan <eashan_007@hotmail.com>
2020-02-23 14:03:23 -0500

    Java Demo

---
+++ Phone.java
@@ -0,0 +1,9 @@
+public class Phone{
+    public static void main(String [] args){
+	int x = 6;
+	int y = 10;
+	int totalPhone = (x+y);
+
+        System.out.println ("Total phones" + totalPhone );
+    }
+}
